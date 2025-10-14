3bff434ba93f35af848b0de19552910013d29a43
Brian Brooks Jr. <brianbrooksjr1@gmail.com>
2025-10-14 12:48:14 -0500

    addDeposit Method added to Leger Class.

--- src/main/java/Ledger.java
+++ src/main/java/Ledger.java
@@ -1,6 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Ledger {
+    //
+    private List<Transaction> transactions = new ArrayList<>();
+    //Deposit UI
+    public void addDeposit (Scanner scanner) {
+        System.out.print ("Enter deposit amount: ");
+        double amount = scanner.nextDouble();
+        System.out.print("Enter deposit description: ");
+        String descriptions = scanner.next();
+
+        //Format the Deposit information
+        transactions.add(new Transaction(amount, description, "Depsit"));
+        System.out.println("Deposit added.");
+
+    }
 
 }
 
