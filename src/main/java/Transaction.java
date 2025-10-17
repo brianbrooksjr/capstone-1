85e606b2bde3b087221af0d822371a3b48e8392b
Brian Brooks Jr. <brianbrooksjr1@gmail.com>
2025-10-17 10:35:26 -0500

    Removed HomeScreen.java

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,32 +1,20 @@
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-
 
 public class Transaction {
-    private final double amount;
-    private String description;
-    private final String vendor;
     private final LocalDateTime dateTime;
+    private final String description;
+    private final String vendor;
+    private final double amount;
 
     public Transaction(LocalDateTime dateTime, String description, String vendor, double amount) {
-        this.dateTime = new dateTime();
+        this.dateTime = dateTime;
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
     }
 
-    public String toCSV() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss"); // Format 2023-04-15|10:13:25
-        return dateTime.format(formatter) + "|" + description + "|" + vendor + amount;//2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00
-    }
-
     public LocalDateTime getDateTime() {
         return dateTime;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public String getVendor() {
@@ -36,5 +24,11 @@ public class Transaction {
     public double getAmount() {
         return amount;
     }
-}
 
+    @Override
+    public String toString() {
+        // Format for CSV: date|time|description|vendor|amount
+        return dateTime.toLocalDate() + "|" + dateTime.toLocalTime() + "|" +
+                description + "|" + vendor + "|" + amount;
+    }
+}
